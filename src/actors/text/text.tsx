c9ca829fe022624c5c226502fe104bcaa69ffdab
Joshua Toth <info@joshuatoth.com>
2020-02-28 12:31:31 +0000

    ayy offsets

--- src/actors/text/text.tsx
+++ src/actors/text/text.tsx
@@ -3,9 +3,9 @@ import { useFrame, useLoader } from 'react-three-fiber'
 import { useCannon, useGameContext } from '../../useCannon'
 import * as CANNON from 'cannon'
 import { stealableProps } from '../../provider/store/game-state'
-import { TextGeometryParameters, FontLoader, Font } from 'three'
+import { TextGeometryParameters, FontLoader, Font, TextGeometry, Vector3, TextBufferGeometry } from 'three'
 
-export const Text: React.FC<stealableProps & { text: string }> = props => {
+export const Text: React.FC<stealableProps & { text: string}> = props => {
   const {
     state: { playerBody },
     dispatch,
@@ -19,13 +19,29 @@ export const Text: React.FC<stealableProps & { text: string }> = props => {
       relativePos: { ...e.body.quaternion },
     })
   }
+  
+  const roboto = require('../../assets/roboto.json')
+  const font: any = new Font(roboto)
+  const config = useMemo(
+    () => { 
+      return({ font, size: 3, height: 0.5 })},
+    [font]
+  )
+  
+  const textGeo = new TextBufferGeometry(props.text, config)
+  textGeo.computeBoundingBox()
+  // textGeo.computeBoundingSphere()
   // This reference will give us direct access to the mesh
-  const ref = useCannon({ mass: 100 }, (body: CANNON.Body) => {
-    body
-    const box = new CANNON.Box(new CANNON.Vec3(props.text.length, 1, 1))
+  const ref = useCannon({ mass: 1 }, (body: CANNON.Body) => {
+    const size = textGeo.boundingBox.getSize(new Vector3())
+    console.log(size)
+    const box = new CANNON.Box(new CANNON.Vec3(size.x, size.y, size.z).scale(0.5))
+    const offsets = [size.x * 0.5, size.y * 0.5, size.z * 0.5]
     body.addShape(box)
 
-    body.position.set(props.position.x, props.position.y, props.position.z)
+    textGeo.translate(-offsets[0], -offsets[1], -offsets[2])
+    // const center = textGeo.boundingSphere.center
+    // body.position.set(size.x + props.offset, size.y, size.z)
   })
 
   // useEffect(() => {
@@ -37,24 +53,9 @@ export const Text: React.FC<stealableProps & { text: string }> = props => {
   //         }, 0)
   //     })
   // }, [playerBody])
-  
-  const roboto = require('../../assets/roboto.json')
-  const font: any = new Font(roboto)
-  const config = useMemo(
-    () => { 
-      return({ font, size: props.text.length, height: 0.5 })},
-    [font]
-  )
   return useMemo(() => {
     return (
-      <mesh ref={ref.ref} castShadow receiveShadow>
-        <textGeometry
-          attach="geometry"
-          args={[
-            props.text,
-            config
-          ]}
-        />
+      <mesh ref={ref.ref} args={[textGeo]} castShadow receiveShadow>
         <meshStandardMaterial
           attach="material"
           roughness={0.5}
--- src/actors/text/texts.tsx
+++ src/actors/text/texts.tsx
@@ -1,18 +1,41 @@
 import React, { useRef, useState, useMemo, useEffect } from 'react'
-import { useCannon, useGameContext } from '../../useCannon'
-import { stealableProps } from '../../provider/store/game-state'
 
 import { Text } from '../text/text'
 import { Vec3 } from 'cannon'
 
-export const Texts: React.FC<{phrase:string, position: Vec3}> = props => {
+export const Texts: React.FC<{ phrase: string; position: Vec3 }> = props => {
 
   return useMemo(() => {
     return (
       <>
-        {props.phrase.split(' ').map((word: string, index: number) => (
-          <Text size={1} text={word} id={index} key={word + index} position={new Vec3(props.position.x + word.length, props.position.y, props.position.z)} />
-        ))}
+        <Text
+            size={1}
+            text={props.phrase}
+            id={1}
+            position={
+              new Vec3(
+                props.position.x,
+                props.position.y,
+                props.position.z
+              )
+            }
+          />
+        {/* {props.phrase.split(' ').map((word: string, index: number) => (
+          <Text
+            offset={index > 0 ? props.phrase[index - 1].length * 8 : 0}
+            size={1}
+            text={word}
+            id={index}
+            key={word + index}
+            position={
+              new Vec3(
+                props.position.x + word.length,
+                props.position.y,
+                props.position.z
+              )
+            }
+          />
+        ))} */}
       </>
     )
   }, [])
